73bb674dc3dc1b0f3d5636ec833aea49ad5c9ac2
kermakovich <kermakovich.laba@solvd.com>
2023-03-09 13:40:38 +0300

    Bugs were fixed #1

--- src/main/java/solvd/laba/ermakovich/hu/repository/DoctorRepository.java
+++ src/main/java/solvd/laba/ermakovich/hu/repository/DoctorRepository.java
@@ -1,11 +1,11 @@
 package solvd.laba.ermakovich.hu.repository;
 
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import solvd.laba.ermakovich.hu.domain.Doctor;
 
 /**
  * @author Ermakovich Kseniya
  */
-public interface DoctorRepository extends ReactiveCrudRepository<Doctor, Long> {
+public interface DoctorRepository extends R2dbcRepository<Doctor, Long> {
 
 }
--- src/main/java/solvd/laba/ermakovich/hu/repository/UserInfoRepository.java
+++ src/main/java/solvd/laba/ermakovich/hu/repository/UserInfoRepository.java
@@ -1,17 +1,15 @@
 package solvd.laba.ermakovich.hu.repository;
 
 import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 import solvd.laba.ermakovich.hu.domain.UserInfo;
 
 /**
  * @author Ermakovich Kseniya
  */
-@Repository
-public interface UserInfoRepository extends ReactiveCrudRepository<UserInfo, Long> {
+public interface UserInfoRepository extends R2dbcRepository<UserInfo, Long> {
 
     @Query("SELECT EXISTS(SELECT 1 FROM user_info ui WHERE ui.external_id::text = :uuid)")
     Mono<Boolean> isExistByExternalId(@Param("uuid") String uuid);
